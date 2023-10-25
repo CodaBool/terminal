4ae87ab30566d60591bd3451baf30fb18b8b129a
Coda Bool <codabool@pm.me>
2023-10-25 17:07:36 -0400

    test out change log extract 1

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+inside_section=false
+changelog_lines=""
+while IFS= read -r line; do
+  if [[ $line == "==="* ]]; then
+    if [ "$inside_section" = false ]; then
+      inside_section=true
+    else
+      break  # Exit the loop
+    fi
+  fi
+  if [ "$inside_section" = true ] && [[ $line == "- "* ]]; then
+    changelog_lines="$changelog_lines$line"$'\n'
+  fi
+done < changelog.txt
+
+echo "changes=\"$changelog_lines\""
